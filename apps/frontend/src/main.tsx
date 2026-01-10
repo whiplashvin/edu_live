561929dd0a37d98752f0d37ea63fc75adbe35d1e
arvindkhoisnam <khoisnamarvind@gmail.com>
2026-01-10 23:59:06 +0530

    whiteboard-fix

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -7,10 +7,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 const query = new QueryClient();
 createRoot(document.getElementById("root")!).render(
-  <RecoilRoot>
-    <QueryClientProvider client={query}>
-      <App />
-      <Toaster />
-    </QueryClientProvider>
-  </RecoilRoot>
+  <StrictMode>
+    <RecoilRoot>
+      <QueryClientProvider client={query}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
+    </RecoilRoot>
+  </StrictMode>
 );
